fd5bc128ba64a47d7ae9f11af598af20cc7b7536
fabogit <fabo.dev.email@gmail.com>
2025-02-13 12:44:53 +0100

    #4
    related data and nested query

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,9 +1,10 @@
 import data from "./data.js";
+// Resolvers for entry points to the graph
 
 export const resolvers = {
 	Query: {
 		authors() {
-			return data.reviews;
+			return data.authors;
 		},
 		author(parent, args) {
 			return data.authors.find((author) => author.id === args.id);
@@ -19,6 +20,25 @@ export const resolvers = {
 		},
 		review(parent, args) {
 			return data.reviews.find((review) => review.id === args.id);
+		}
+	},
+
+	Game: {
+		reviews(parent, args) {
+			return data.reviews.filter((review) => review.game_id === parent.id);
+		}
+	},
+	Author: {
+		reviews(parent, args) {
+			return data.reviews.filter((review) => review.author_id === parent.id);
+		}
+	},
+	Review: {
+		author(parent, args) {
+			return data.authors.find((author) => author.id === parent.author_id);
+		},
+		game(parent, args) {
+			return data.games.find((game) => game.id === parent.game_id);
 		}
 	}
 };
